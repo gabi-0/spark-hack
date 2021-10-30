1e3adda4cb0e5eb57007eb877e8f3fb2dfb9e267
gabi-0 <gmihalcea5@gmail.com>
2021-10-30 20:31:50 +0300

    set profile

--- public/js/create-profile.js
+++ public/js/create-profile.js
@@ -1,38 +1,91 @@
 
-city_data[1];
-
-function csvToArray(str, delimiter = ",") {
-	// slice from start of text to the first \n index
-	// use split to create an array from string by delimiter
-	const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-  
-	// slice from \n index + 1 to the end of the text
-	// use split to create an array of each csv value row
-	const rows = str.slice(str.indexOf("\n") + 1).split("\n");
-  
-	// Map the rows
-	// split values from each row into an array
-	// use headers.reduce to create an object
-	// object properties derived from headers:values
-	// the object passed as an element of the array
-	const arr = rows.map(function (row) {
-	  const values = row.split(delimiter);
-	  const el = headers.reduce(function (object, header, index) {
-		object[header] = values[index];
-		return object;
-	  }, {});
-	  return el;
-	});
-  
-	// return the array
-	return arr;
-  }
-
 cities = city_data.split(";");
 
-function city_find() {
+function city_find(str) {
 	res_arr = new Array();
+
+	l = str.length;
+	str = str.toLowerCase();
 	cities.forEach(el => {
-		if()
+		if(el.substr(0,l).localeCompare(str) == 0) {
+			aux_str = el.split(",")[0];
+			res_arr.push(aux_str[0].toUpperCase() + aux_str.substr(1));
+		}
+	});
+	return res_arr;
+}
+
+/*city_sugg = document.getElementById("city-suggestions");
+
+document.getElementById("city-input").addEventListener('input', function(e) {
+	res = city_find(e.target.value);
+	city_sugg.textContent = res;
+});*/
+
+
+function autocomplete(inp) {
+
+	inp.addEventListener("input", function(e) {
+		var a, b, i, val = this.value.toLowerCase();
+		var l = val.length;
+
+		closeAllLists();
+		if (!val) { return false;}
+	
+		a = document.createElement("DIV");
+		a.setAttribute("id", this.id + "autocomplete-list");
+		a.setAttribute("class", "autocomplete-items");
+
+		this.parentNode.appendChild(a);
+
+		cities.forEach(el => {
+		  if(el.substr(0,l).localeCompare(val) == 0) {
+			  
+			aux_str = el.split(",")[0].toUpperCase();
+
+			b = document.createElement("DIV");
+			b.innerHTML = "<strong>" + val.toUpperCase() + "</strong>";
+			b.innerHTML += aux_str.substr(l);
+
+			b.innerHTML += "<input type='hidden' value='" + aux_str + "'>";
+			b.addEventListener("click", function(e) {
+				inp.value = this.getElementsByTagName("input")[0].value;
+				closeAllLists();
+			});
+			a.appendChild(b);
+		  }
+		});
+	});
+
+	function closeAllLists(elmnt) {
+	  var x = document.getElementsByClassName("autocomplete-items");
+	  for (var i = 0; i < x.length; i++) {
+		if (elmnt != x[i] && elmnt != inp) {
+			x[i].parentNode.removeChild(x[i]);
+	  	}
+	  }
+	}
+
+	document.addEventListener("click", function (e) {
+		closeAllLists(e.target);
 	});
 }
+
+autocomplete(document.getElementById("homecity-input"));
+autocomplete(document.getElementById("desiredcity-input"));
+
+document.getElementById("nextpg").addEventListener("click", function(e) {
+	if(document.getElementById("homecity-input").value == '') return;
+
+	document.getElementById("home-form").style.display = "none";
+	document.getElementById("desired-form").style.display = "block";
+	document.getElementById("image-bg").src='./img/desired.jpg'
+});
+
+sendbtn = document.getElementById("sendbtn");
+sendbtn.addEventListener("click", function(e) {
+	if(document.getElementById("desiredcity-input").value == '') return;
+
+	sendbtn.innerHTML = "Saving <img src='./img/load.gif'>";
+	sendbtn.setAttribute("disabled", "true");
+});
